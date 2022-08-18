850746f9d79be0a9a85c76e5a0c73f5e82195d05
demonlord <89049629+demonlord6661@users.noreply.github.com>
2022-08-17 22:47:27 -0700

    major
    long awaited changes being made to code to account for requirements.

--- backbone.js
+++ backbone.js
@@ -9,10 +9,18 @@ function delayedMessage() {
   timeoutID = setTimeout(setOutput, 2*1000, 'For the most of Summer it was really bland sleep wake up go to work and so on but what i found fun was the free time it generated over the summer time was like an infinte loop coding error and it was refreshing to have time to myself however short lived it was.');
 }
 
-var img = document.createElement('img');
-img.src = 'img/grass-backyard.png';
-
-var div = document.getElementById('x');
-div.appendChild(img);
-
-
+let elementsArray = document.querySelectorAll(".tile");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn);
+function fadeIn() {
+  for (var i = 0; i < elementsArray.length; i++) {
+    var elem = elementsArray[i]
+    var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+    if (distInView < 0) {
+      elem.classList.add("inView");
+    } else {
+      elem.classList.remove("inView");
+    }
+  }
+}
+fadeIn();
