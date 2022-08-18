5be8097a7a78a9f7bb8c3344af1e3d4af929469f
demonlord <89049629+demonlord6661@users.noreply.github.com>
2022-08-17 23:45:44 -0700

    Total work
    Most of the work was roughly done but it went with the requirements and had more than a good amount of js and scalability for mobile devices.

--- backbone.js
+++ backbone.js
@@ -3,12 +3,12 @@ let timeoutID;
 function setOutput(outputContent) {
   document.querySelector('#output').textContent = outputContent;
 }
-
+//sun button main body
 function delayedMessage() {
   setOutput('');
-  timeoutID = setTimeout(setOutput, 2*1000, 'For the most of Summer it was really bland sleep wake up go to work and so on but what i found fun was the free time it generated over the summer time was like an infinte loop coding error and it was refreshing to have time to myself however short lived it was.');
+  timeoutID = setTimeout(setOutput, 2*1000, 'For the most of Summer it was really bland, sleep wake up go to work and so on but what i found fun was the free time it generated over the summer time was like an infinite loop coding error and it was refreshing to have time to myself.');
 }
-
+//code under here is for the fade in of the boxes and time control
 let elementsArray = document.querySelectorAll(".tile");
 console.log(elementsArray);
 window.addEventListener('scroll', fadeIn);
